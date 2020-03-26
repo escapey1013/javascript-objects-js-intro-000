dcf87b98f6be7b01ed3c12ada234d702b4866255
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 12:38:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,31 @@
-var playlist = {Singer1: 'song1'};
 
-function updatePlaylist(object, artistName, song) {
-  object[artistName] = song;
-  return object;
-}
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
-  return playlist
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var playlist = {Singer1: 'song1'};
+//
+// function updatePlaylist(object, artistName, song) {
+//   object[artistName] = song;
+//   return object;
+// }
+//
+// function removeFromPlaylist(playlist, artistName) {
+//   delete playlist[artistName];
+//   return playlist
+// }
